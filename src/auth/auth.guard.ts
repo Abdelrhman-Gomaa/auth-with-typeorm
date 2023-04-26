57a5de47b92a69b92deba7b1b4eba51dc5982ef5
Adbelrhman Gomaa <abdelrhman.gomaa@baianat.com>
2023-04-26 12:07:42 +0200

    feat: auth gaurd and current User Decorator

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,25 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import * as jwt from 'jsonwebtoken';
+import { CanActivate, ExecutionContext, Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { User } from 'src/user/models/user.model';
+import { TokenPayload } from './auth-token-payload.interface';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(
+        @InjectRepository(User)
+        private userRepo: Repository<User>,
+    ) { }
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest();
+        const token = request.header('Authorization').split(' ')[1];
+        if (!token) throw new UnauthorizedException('Need Token');
+        const { userId } = <TokenPayload>jwt.verify(token, process.env.JWT_SECRET);
+        if (!userId) throw new UnauthorizedException(`Invalid token`);
+        const user = await this.userRepo.findOne({ where: { id: userId } });
+        if (!user) throw new UnauthorizedException(`Can't Find User`);
+        return true;
+    }
+}
