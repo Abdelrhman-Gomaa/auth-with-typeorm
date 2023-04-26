b5b52fe04ede83eb0d19704291e78b63b3131b88
Adbelrhman Gomaa <abdelrhman.gomaa@baianat.com>
2023-04-26 10:38:55 +0200

    feat: swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth With TypeOrm')
+    .setDescription('The Authentication API With TypeOrm')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('auth-with-typeorm', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
